fb29d9289b5d69760059b4f040fe017e6af7eae9
Lukas Ahrenberg <lukas@ahrenberg.se>
2017-11-29 23:39:58 +0200

    Added header.

--- ink-mode.el
+++ ink-mode.el
@@ -1,3 +1,27 @@
+;; ink-mode.el --- Major mode for editing inklestudio's ink files.
+
+;; Copyright (C) 2017 Lukas Ahrenberg
+;;
+;; Author:   Lukas Ahrenberg <lukas@ahrenberg.se>
+;; Created:  November, 2017
+;; Version:  0,1
+;; Keywords: ink-mode
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;; Notes: Only regexp-based highlighting (and syntax table) right now, meaning
+;; that it currently can not capture any context.
 
 (require 'rx)
 
